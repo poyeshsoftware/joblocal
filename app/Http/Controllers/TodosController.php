1ffd9284c9cb1bebec097b3bbd5720f288f06c27
Barzan <payamprivate2@gmail.com>
2020-09-03 22:25:20 +0430

    activating the elequent , and making TodosController.php

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-class ExampleController extends Controller
+class TodosController extends Controller
 {
     /**
      * Create a new controller instance.
@@ -14,5 +14,7 @@ class ExampleController extends Controller
         //
     }
 
-    //
+    public function index(){
+
+    }
 }
